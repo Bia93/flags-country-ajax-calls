11bafa2f25531023b0072c7c35b53089bc29d442
Bia Grigore <biancag.grigore@gmail.com>
2024-05-14 11:46:05 +0300

    fetch and promises with explanation

--- script.js
+++ script.js
@@ -37,8 +37,7 @@ getCountryData("romania"); */
 //it s shoes the non blocking behaviour
 //whenever the first one arrives will then fore the load event first
 //if we want in a order,we have to chain the requests
-
-//-----WE WILL CREATE A SEQUENCE OF AJAX CALLS, SO THE SECOND ONE RUNS ONLY AFTER THE FIRST ONE HAS FINISHED (adica portugal sa apara prima si apoi romania)
+//--------WE WILL CREATE A SEQUENCE OF AJAX CALLS, SO THE SECOND ONE RUNS ONLY AFTER THE FIRST ONE HAS FINISHED (adica portugal sa apara prima si apoi romania)
 
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
@@ -59,7 +58,7 @@ const renderCountry = function (data, className = "") {
   countriesContainer.insertAdjacentHTML("beforeend", html);
   countriesContainer.style.opacity = 1;
 };
-const getCountryandNeighbour = function (country) {
+/*const getCountryandNeighbour = function (country) {
   //AJAX call country 1
   const request = new XMLHttpRequest(); //the old way
   //i want to see how AJAX CALLS USE TO BE handled with events an callback functions.
@@ -90,3 +89,46 @@ const getCountryandNeighbour = function (country) {
   });
 };
 getCountryandNeighbour("romania"); //AJAX CALLS
+//  ---- in   ES6, to escape from callback hell i mean to have more contries, and to not hav the
+//triangle shape, we use PROMISES*/
+
+//-------
+
+/*const request = new XMLHttpRequest(); //the old way
+  //i want to see how AJAX CALLS USE TO BE handled with events an callback functions.
+  request.open("GET", `https://restcountries.com/v3/name/${country}`); //GET is for getting the data;api- we need the url to which  tha ajax call should actually be made
+  request.send(); //send off the request; fetching the data in the background;once that it is done it will emit the addEvent Listner*/
+// in loc de ce mai sus( face parte din script.js), scriem cu promises
+
+const getCountryData = function (country) {
+  //1. calling the fetch will immediatly return a promise; in the beginning the prmoise is still pending because the asychronous task of getting the data, is still running in the background
+  fetch(`https://restcountries.com/v3/name/${country}`)
+    .then(function (
+      //2.we use THEN method for handling the fulfilled state,its  avaible in all promises
+      response
+    ) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      renderCountry(data[0]);
+    });
+};
+getCountryData("portugal");
+//3.the argument from the callbackfunction of then, as the resulting value of the fulfilled promises
+//4.to be able to read the data from body: ReadableStream we need to call the json method on the response
+//the json method from the response.json()- is a method that is available
+//on all the response objects that is coming from the fetch function; the problem here is that
+// the json is actually also an asynchronous function,meaning that it also return a new promise. we need to return the promise( we put a return).
+//for that having a new promise we have to return it, and we use then method
+
+//----THE SIMPLIFY VERSION
+/*const getCountryData = function (country) {
+  //1. calling the fetch will immediatly return a promise; in the beginning the prmoise is still pending because the asychronous task of getting the data, is still running in the background
+  fetch(`https://restcountries.com/v3/name/${country}`)
+    .then(response => response.json());
+    .then(data=>renderCountry(data[0]));
+   
+};
+getCountryData("portugal");*/
